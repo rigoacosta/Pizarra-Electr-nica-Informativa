9ee42faf4dd88af2b140caddedc8706dcc18b7c8
rigoacosta <ragonzalez@uclv.cu>
2017-06-20 16:08:39 -0400

    Add files via upload

---
+++ Pizarra C#/MainForm.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace Pizarra
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.about = new System.Windows.Forms.Button();
+			this.label11 = new System.Windows.Forms.Label();
+			this.comboBox3 = new System.Windows.Forms.ComboBox();
+			this.conButton = new System.Windows.Forms.Button();
+			this.comboBox2 = new System.Windows.Forms.ComboBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.button2 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label8 = new System.Windows.Forms.Label();
+			this.groupBox3 = new System.Windows.Forms.GroupBox();
+			this.button5 = new System.Windows.Forms.Button();
+			this.textBox6 = new System.Windows.Forms.TextBox();
+			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.btnHora = new System.Windows.Forms.Button();
+			this.groupBox1.SuspendLayout();
+			this.groupBox3.SuspendLayout();
+			this.groupBox2.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.about);
+			this.groupBox1.Controls.Add(this.label11);
+			this.groupBox1.Controls.Add(this.comboBox3);
+			this.groupBox1.Controls.Add(this.conButton);
+			this.groupBox1.Controls.Add(this.comboBox2);
+			this.groupBox1.Controls.Add(this.label3);
+			this.groupBox1.Controls.Add(this.label1);
+			this.groupBox1.Controls.Add(this.comboBox1);
+			resources.ApplyResources(this.groupBox1, "groupBox1");
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.TabStop = false;
+			// 
+			// about
+			// 
+			resources.ApplyResources(this.about, "about");
+			this.about.Name = "about";
+			this.about.UseVisualStyleBackColor = true;
+			this.about.Click += new System.EventHandler(this.AboutClick);
+			// 
+			// label11
+			// 
+			resources.ApplyResources(this.label11, "label11");
+			this.label11.Name = "label11";
+			// 
+			// comboBox3
+			// 
+			this.comboBox3.FormattingEnabled = true;
+			resources.ApplyResources(this.comboBox3, "comboBox3");
+			this.comboBox3.Name = "comboBox3";
+			// 
+			// conButton
+			// 
+			resources.ApplyResources(this.conButton, "conButton");
+			this.conButton.Name = "conButton";
+			this.conButton.UseVisualStyleBackColor = true;
+			this.conButton.Click += new System.EventHandler(this.ConButtonClick);
+			// 
+			// comboBox2
+			// 
+			this.comboBox2.FormattingEnabled = true;
+			resources.ApplyResources(this.comboBox2, "comboBox2");
+			this.comboBox2.Name = "comboBox2";
+			// 
+			// label3
+			// 
+			resources.ApplyResources(this.label3, "label3");
+			this.label3.Name = "label3";
+			// 
+			// label1
+			// 
+			resources.ApplyResources(this.label1, "label1");
+			this.label1.Name = "label1";
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			resources.ApplyResources(this.comboBox1, "comboBox1");
+			this.comboBox1.Name = "comboBox1";
+			// 
+			// button2
+			// 
+			resources.ApplyResources(this.button2, "button2");
+			this.button2.Name = "button2";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			// 
+			// textBox1
+			// 
+			resources.ApplyResources(this.textBox1, "textBox1");
+			this.textBox1.Name = "textBox1";
+			// 
+			// label8
+			// 
+			resources.ApplyResources(this.label8, "label8");
+			this.label8.Name = "label8";
+			// 
+			// groupBox3
+			// 
+			this.groupBox3.Controls.Add(this.button5);
+			this.groupBox3.Controls.Add(this.textBox6);
+			this.groupBox3.Controls.Add(this.label8);
+			resources.ApplyResources(this.groupBox3, "groupBox3");
+			this.groupBox3.Name = "groupBox3";
+			this.groupBox3.TabStop = false;
+			this.groupBox3.Enter += new System.EventHandler(this.GroupBox3Enter);
+			// 
+			// button5
+			// 
+			resources.ApplyResources(this.button5, "button5");
+			this.button5.Name = "button5";
+			this.button5.UseVisualStyleBackColor = true;
+			this.button5.Click += new System.EventHandler(this.Button5Click);
+			// 
+			// textBox6
+			// 
+			this.textBox6.BackColor = System.Drawing.Color.White;
+			resources.ApplyResources(this.textBox6, "textBox6");
+			this.textBox6.Name = "textBox6";
+			this.textBox6.ReadOnly = true;
+			// 
+			// groupBox2
+			// 
+			this.groupBox2.Controls.Add(this.label2);
+			this.groupBox2.Controls.Add(this.btnHora);
+			this.groupBox2.Controls.Add(this.textBox1);
+			this.groupBox2.Controls.Add(this.button2);
+			resources.ApplyResources(this.groupBox2, "groupBox2");
+			this.groupBox2.Name = "groupBox2";
+			this.groupBox2.TabStop = false;
+			// 
+			// label2
+			// 
+			this.label2.BackColor = System.Drawing.Color.Transparent;
+			resources.ApplyResources(this.label2, "label2");
+			this.label2.Name = "label2";
+			// 
+			// btnHora
+			// 
+			resources.ApplyResources(this.btnHora, "btnHora");
+			this.btnHora.Name = "btnHora";
+			this.btnHora.UseVisualStyleBackColor = true;
+			this.btnHora.Click += new System.EventHandler(this.btnHoraClick);
+			// 
+			// MainForm
+			// 
+			resources.ApplyResources(this, "$this");
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.groupBox2);
+			this.Controls.Add(this.groupBox3);
+			this.Controls.Add(this.groupBox1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.MaximizeBox = false;
+			this.Name = "MainForm";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainFormFormClosing);
+			this.Load += new System.EventHandler(this.MainFormLoad);
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox3.ResumeLayout(false);
+			this.groupBox3.PerformLayout();
+			this.groupBox2.ResumeLayout(false);
+			this.groupBox2.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+		
+		private System.Windows.Forms.ComboBox comboBox3;
+		private System.Windows.Forms.Label label11;
+		private System.Windows.Forms.TextBox textBox6;
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.GroupBox groupBox3;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button conButton;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.ComboBox comboBox2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.GroupBox groupBox2;
+		private System.Windows.Forms.Button btnHora;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button about;
+		
+		
+	}
+}
---
+++ Pizarra C#/MainForm.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Drawing;
+using System.IO;
+using System.IO.Ports;
+using System.Text;
+using System.Windows.Forms;
+
+
+namespace Pizarra
+{	
+	public partial class MainForm : Form
+	{
+		SerialPort sp1 = null;
+		
+		string cad = "";			
+		
+		public MainForm()
+		{			
+			InitializeComponent();		
+		}
+		
+		public void tx_char(SerialPort SBUF, byte[] x)
+		{
+			if (SBUF == null ) return;			
+			if ( SBUF.IsOpen == false )
+				SBUF.Open();		
+			
+			SBUF.Write(x, 0, x.Length);	
+		}		
+				
+		void MainFormLoad(object sender, EventArgs e)
+		{
+			string[] ports = SerialPort.GetPortNames(); 
+			comboBox1.Items.Clear();
+
+						
+			foreach(string port in ports) 
+			{ 
+				comboBox1.Items.Add(port); 
+			} 
+			
+			if (comboBox1.Items.Count > 0)
+			{
+				comboBox1.Text = comboBox1.Items[0].ToString();
+			}
+			else
+			{
+				conButton.Enabled = false;
+			}
+			
+			comboBox2.Items.Add("1200");
+			comboBox2.Items.Add("2400"); 
+			comboBox2.Items.Add("4800"); 
+			comboBox2.Items.Add("9600"); 
+			comboBox2.Items.Add("19200"); 
+			comboBox2.Text = "9600"; 	
+			comboBox3.Items.Add(Parity.Even.ToString()); 
+			comboBox3.Items.Add(Parity.Mark.ToString()); 
+			comboBox3.Items.Add(Parity.None.ToString()); 
+			comboBox3.Items.Add(Parity.Odd.ToString()); 
+			comboBox3.Items.Add(Parity.Space.ToString()); 
+			comboBox3.Text = "None";
+		}							
+		
+		private void port_DataReceived(object sender, SerialDataReceivedEventArgs e) 
+		{ 			 					
+			cad = sp1.ReadExisting();			
+		}
+		
+		void MainFormFormClosing(object sender, FormClosingEventArgs e)
+		{
+			if ( sp1 != null && sp1.IsOpen )
+				sp1.Close();			
+		}
+		
+		void ConButtonClick(object sender, EventArgs e)
+		{
+			if ( sp1 == null || sp1.IsOpen == false )
+			{
+				Parity Paridad = Parity.None; 
+				switch (comboBox3.Text)
+				{
+					case "Even" : Paridad = Parity.Even; 
+								  break;								  
+					case "Mark" : Paridad = Parity.Mark; 
+								  break;
+					case "Odd"  : Paridad = Parity.Odd; 
+								  break;
+					case "Space": Paridad = Parity.Space; 					
+								  break;																	  
+					default		: Paridad = Parity.None;
+								  break;
+				}													
+				sp1 = new SerialPort(comboBox1.Text,Convert.ToInt16(comboBox2.Text),Paridad,8,StopBits.One);
+				sp1.DataReceived += new SerialDataReceivedEventHandler(port_DataReceived); 
+				sp1.Open();
+				comboBox1.Enabled = false;
+				comboBox2.Enabled = false;
+				comboBox3.Enabled = false;
+				textBox1.Enabled = true;
+				button2.Enabled = true;
+				btnHora.Enabled = true;
+				conButton.Text = "Desconectar";
+			}
+			else
+			{				
+				sp1.Close();
+				comboBox1.Enabled = true;
+				comboBox2.Enabled = true;
+				comboBox3.Enabled = true;
+				textBox1.Enabled = false;
+				button2.Enabled = false;
+				btnHora.Enabled = false;
+				conButton.Text = "Conectar";
+			}
+		}
+
+		void Button2Click(object sender, EventArgs e)
+		{
+			if ( sp1 != null && sp1.IsOpen == true && textBox1.Text.Length != 0 )
+			{			
+				
+				string f  = textBox1.Text.ToString();
+				byte[] frame = null;
+				frame = new byte[f.Length + 4];
+				
+				frame[0] = 0x3A;
+				frame[1] = 0x43;
+				frame[2] = (byte) f.Length;
+				int k = 3;
+				for(int i = 0; i < f.Length ; i++)
+				{
+					switch(f[i])
+					{
+						case '-' : frame[k++] = (byte)0x10; break;
+		                case 'ñ' : frame[k++] = (byte)0x11; break;
+                		case 'Ñ' : frame[k++] = (byte)0x11; break;
+                		case ' ' : frame[k++] = (byte)0x12; break;
+						default: frame[k++] = (byte)f[i]; break;	
+					}
+				}
+				frame[k++] = 0x0D;
+				for(int i = 0; i < frame.Length ; i++)
+				{
+					textBox6.AppendText(frame[i].ToString());
+				}
+				
+				
+				this.tx_char(sp1,frame);
+			}	 						
+		}
+		
+		void Button5Click(object sender, System.EventArgs e)
+		{
+			textBox6.Clear();
+		}
+	
+		void btnHoraClick(object sender, EventArgs e)
+		{
+			DateTime tiempo = DateTime.Now;
+			byte[] frame = null;
+			
+			frame = new byte[11];
+			
+			frame[0] = 0x3A;
+			frame[1] = (byte)'H';
+	
+			frame[2] = (byte)(tiempo.Second%60);
+			frame[3] = (byte)tiempo.Minute;
+			frame[4] = (byte)tiempo.Hour;
+			if(frame[4]  > 12 || frame[4] == 0 )
+			{
+				if(frame[4] == 0)
+				{
+					frame[4] = 12;
+					frame[9] = (byte)'P';
+				}
+				else
+				{
+					frame[4] -= 12;
+					frame[9] = (byte)'P';
+				}
+			}
+			else
+			{
+				frame[9] = (byte)'A';
+			}
+
+			
+			frame[5] = (byte)(tiempo.DayOfWeek + 1);
+			frame[6] = (byte)(tiempo.Day);
+			frame[7] = (byte)(tiempo.Month);
+			frame[8] = (byte)(tiempo.Year%100);
+			frame[10] = 0x0D;
+			
+
+			for(int i = 0; i < frame.Length ; i++)
+			{
+				textBox6.AppendText(frame[i].ToString());
+			}
+			
+			this.tx_char(sp1,frame);
+		}
+		
+		void AboutClick(object sender, System.EventArgs e)
+		{
+			MessageBox.Show("Pizarra Electrónica Informativa" + "\nUCLV-FIE" +
+			                "\t14/06/2017" + "\nContacto: ragonzalez@uclv.cu ");
+		}
+		void GroupBox3Enter(object sender, EventArgs e)
+		{
+	
+		}
+
+	}	
+}
